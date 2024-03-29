b24b55ef9d9e45d95c77253c131bfce633825094
Kadyrbaev <sadyr_9494@mail.ru>
2024-03-29 13:37:53 +0600

    input beksultan

---
+++ src/components/InfutForm.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Button from './Button'
+
+const InfutForm = () => {
+  return (
+    <div>
+      <input type="text" />
+      <input type="text" />
+      <Button/>
+    </div>
+  )
+}
+
+export default InfutForm
